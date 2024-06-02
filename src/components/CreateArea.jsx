4eab72a1bd39a82cb85f92b46a130731c23b13a8
Tykunal <kunal2021CSAI065@abesit.edu.in>
2024-06-02 12:58:55 +0530

    modified material ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -3,25 +3,32 @@ import AddIcon from "@mui/icons-material/Add";
 import { Fab } from "@mui/material";
 import { Zoom } from "@mui/material";
 
-
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+  function expand(){
+    setExpanded(true);
+  }
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          onChange={props.titleChange}
-          value={props.titleVal}
-          placeholder="Title"
-        />
-        <textarea
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={props.titleChange}
+            value={props.titleVal}
+            placeholder="Title"
+          />
+        )}
+        <textarea onClick={expand}
           value={props.descVal}
           onChange={props.descChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded?3:1}
         />
-        <button onClick={props.click}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={props.click}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
